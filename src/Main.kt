553e5a322b933b393527bd0bd890a48e61add1b1
luka_chinchaladze <luka.chinchaladze.1@btu.edu.ge>
2021-04-07 21:51:07 +0400

    Add second homework

---
+++ src/Main.kt
@@ -0,0 +1,43 @@
+import kotlin.jvm.internal.PropertyReference0Impl
+
+fun main(){
+    var num1 = 5F;
+    var num2 = 12F;
+
+    var obj = Fraction(num1,num2,"1/4");
+
+    println("Fraction = " + obj.Fract())
+
+    println("${num1} + ${num2} = "+obj.Addition());
+    println("${num1} - ${num2} = "+obj.Subtraction());
+    println("${num1} * ${num2} = "+obj.Multiplication());
+    println("${num1} / ${num2} = "+obj.Division());
+}
+
+class Fraction(private var num1: Float, private var num2: Float, private var fract: String){
+
+    fun Addition(): Float{
+        return num1 + num2;
+    }
+
+    fun Subtraction(): Float{
+        return num1 - num2;
+    }
+
+    fun Multiplication(): Float{
+        return num1 * num2;
+    }
+
+    fun Division(): Float{
+        return num1 / num2;
+    }
+
+    fun Fract(): Double {
+        if (fract.contains("/")) {
+            val rat: List<String> = fract.split("/")
+            return rat[0].toDouble() / rat[1].toDouble()
+        }
+        return 0.0
+    }
+
+}
